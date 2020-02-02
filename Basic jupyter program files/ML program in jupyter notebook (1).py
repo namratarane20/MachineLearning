bc1fc4f22e6e0e0619508507d3a76692940fcac2
Namrata Ashok <namrata.ashok@impelsys.com>
2020-02-02 15:57:52 +0530

    jupyter notebook python programs to plot the graph using matplotlib

---
+++ Basic jupyter program files/ML program in jupyter notebook (1).py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+print(a)
+
+
+# In[2]:
+
+
+print('hello welcome to jupyter noteboot')
+
+
+# In[3]:
+
+
+print('hello welcome to jupyter noteboot')
+print(a)
+print('data is ')
+val= 20
+print(val)
+
+
+# 40-50
+
+# In[6]:
+
+
+40 - 50 
+
+
+# In[10]:
+
+
+30 / 2
+
+
+# In[14]:
+
+
+name = 'namrata ','namu'
+
+
+# In[16]:
+
+
+name ,age = 'namrata',23
+
+
+# In[17]:
+
+
+name
+
+
+# In[18]:
+
+
+age
+
+
+# In[ ]:
+
+
+
+
